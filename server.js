cbba6b3ecb4be05f0d439d95b28aea23f9508dd8
jrschmidt <jrschmidt56@gmail.com>
2020-04-20 08:24:56 -0700

    load 'state' and 'delegate' data from external files

--- server.js
+++ server.js
@@ -1,223 +1,11 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express');
+const fs = require('fs');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
-// Data constants
-const stateData = {
-  AZ: {
-    id: 'az',
-    code: 'AZ',
-    name: 'Arizona',
-    allotedDelegates: 79
-  },
 
-  CA: {
-    id: 'ca',
-    code: 'CA',
-    name: 'California',
-    allotedDelegates: 494
-  },
-
-  CO: {
-    id: 'co',
-    code: 'CO',
-    name: 'Colorado',
-    allotedDelegates: 80
-  },
-
-  KS: {
-    id: 'ks',
-    code: 'KS',
-    name: 'Kansas',
-    allotedDelegates: 45
-  },
-
-  NV: {
-    id: 'nv',
-    code: 'NV',
-    name: 'Nevada',
-    allotedDelegates: 48
-  }
-};
-
-const delegateData = {
-  az001: {
-    id: 'az001',
-    state: 'AZ',
-    name: 'Debora Anderson',
-    votes: []
-  },
-
-  az002: {
-    id: 'az002',
-    state: 'AZ',
-    name: 'John Bailey',
-    votes: []
-  },
-
-  az003: {
-    id: 'az003',
-    state: 'AZ',
-    name: 'Ana Jimenez',
-    votes: []
-  },
-
-  az004: {
-    id: 'az004',
-    state: 'AZ',
-    name: 'Brian Nederfeld',
-    votes: []
-  },
-
-  az005: {
-    id: 'az005',
-    state: 'AZ',
-    name: 'Thomas Ramirez',
-    votes: []
-  },
-
-  ca001: {
-    id: 'ca001',
-    state: 'CA',
-    name: 'Joshua Billings',
-    votes: []
-  },
-
-  ca002: {
-    id: 'ca002',
-    state: 'CA',
-    name: 'Virginia Davis',
-    votes: []
-  },
-
-  ca003: {
-    id: 'ca003',
-    state: 'CA',
-    name: 'Wanda Harris',
-    votes: []
-  },
-
-  ca004: {
-    id: 'ca004',
-    state: 'CA',
-    name: 'Henry Kingston',
-    votes: []
-  },
-
-  ca005: {
-    id: 'ca005',
-    state: 'CA',
-    name: 'Valerie Williams',
-    votes: []
-  },
-
-  co001: {
-    id: 'co001',
-    state: 'CO',
-    name: 'Eddie Danforth',
-    votes: []
-  },
-
-  co002: {
-    id: 'co002',
-    state: 'CO',
-    name: 'Brenda Gordon',
-    votes: []
-  },
-
-  co003: {
-    id: 'co003',
-    state: 'CO',
-    name: 'Yvonne Londres',
-    votes: []
-  },
-
-  co004: {
-    id: 'co004',
-    state: 'CO',
-    name: 'James Neff',
-    votes: []
-  },
-
-  co005: {
-    id: 'co005',
-    state: 'CO',
-    name: 'Sandra Parker',
-    votes: []
-  },
-
-  ks001: {
-    id: 'ks001',
-    state: 'KS',
-    name: 'Timothy Chandler',
-    votes: []
-  },
-
-  ks002: {
-    id: 'ks002',
-    state: 'KS',
-    name: 'Martha Fogel',
-    votes: []
-  },
-
-  ks003: {
-    id: 'ks003',
-    state: 'KS',
-    name: 'Karen Newman',
-    votes: []
-  },
-
-  ks004: {
-    id: 'ks004',
-    state: 'KS',
-    name: 'Larry Perkins',
-    votes: []
-  },
-
-  ks005: {
-    id: 'ks005',
-    state: 'KS',
-    name: 'Robert Quinn',
-    votes: []
-  },
-
-  nv001: {
-    id: 'nv001',
-    state: 'NV',
-    name: 'Jeremiah Adams',
-    votes: []
-  },
-
-  nv002: {
-    id: 'nv002',
-    state: 'NV',
-    name: 'Paul Conner',
-    votes: []
-  },
-
-  nv003: {
-    id: 'nv003',
-    state: 'NV',
-    name: 'Brian Edwards',
-    votes: []
-  },
-
-  nv004: {
-    id: 'nv004',
-    state: 'NV',
-    name: 'Wilma Miller',
-    votes: []
-  },
-
-  nv005: {
-    id: 'nv005',
-    state: 'NV',
-    name: 'Pamela Young',
-    votes: []
-  }
-};
-
-var schema = buildSchema(`
+//Define Graphql schema
+const schema = buildSchema(`
   type Query {
     states: [String]
     state(stateCode: StateCode): State
@@ -246,7 +34,9 @@ var schema = buildSchema(`
   }
 `);
 
-var root = {
+
+// Define Graphql resolvers
+const root = {
 
   states: () => {
     return ['AZ', 'CA', 'CO', 'KS', 'NV'];
@@ -262,8 +52,6 @@ var root = {
     };
   },
 
-// delegate(id: ID): Delegate
-
   delegate: (args) => {
     const del = delegateData[args.id];
     return {
@@ -273,14 +61,43 @@ var root = {
     };
   }
 
+  // const stateDataJson = JSON.stringify(stateData);
+  // fs.writeFileSync('./state-data.json', stateDataJson);
+  // console.log('Writing state data to file state-data.json');
+
+  // const delegateDataJson = JSON.stringify(delegateData);
+  // fs.writeFileSync('./delegate-data.json', delegateDataJson);
+  // console.log('Writing delegate data to file delegate-data.json');
+
 };
 
+
+// Load data from JSON files
+try {
+  const stateDataJson = fs.readFileSync('./state-data.json');
+  stateData = JSON.parse(stateDataJson);
+}
+catch(error) {
+  console.log(error)
+}
+
+try {
+  const delegateDataJson = fs.readFileSync('./delegate-data.json');
+  delegateData = JSON.parse(delegateDataJson);
+}
+catch(error) {
+  console.log(error)
+}
+
+
+// Initialize Express server with Graphql middleware
 var app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
 }));
+
 
 app.listen(4000);
 console.log('Running the RemDel API server at http://localhost:4000/graphql');
